234d381acfc48f8a0030d3efb993de4b4f499d61
T-Kakugawa <kakugawatakayuki@gmail.com>
2023-08-09 15:12:10 +0900

    some chgs in index main

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-//画像を変更するやつ
+//画像を変更する関数
 function changeImage() {
+    //画像のソース配列
     let images = ["../CompanyHPwithFlexBox/resources/images/shiba.jpeg",
+        "../CompanyHPwithFlexBox/resources/images/shibaX.jpeg",
         "../CompanyHPwithFlexBox/resources/images/shiba3.jpeg",
-        "../CompanyHPwithFlexBox/resources/images/shiba4.jpeg",
-        "../CompanyHPwithFlexBox/resources/images/草原.jpeg"];
+        "../CompanyHPwithFlexBox/resources/images/shiba4.jpeg"];
+    //現在表示されている画像のインデックスを取得
+    let src = document.getElementById("image").getAttribute("src");
+    let previousIndex;
+    for(let i=0; i<images.length;i++) {
+        if(src===images[i]) {
+            previousIndex = i;
+        }
+    }
+    //現在のインデックスと違う番号を選択
+    let randomIndex;
+    do {
+        randomIndex = Math.floor(Math.random() * images.length)
+    } while(randomIndex === previousIndex)
+
     const image = document.getElementById("image");
-    let randomIndex = Math.floor(Math.random() * images.length);
     image.src = images[randomIndex];
 }
 const image = document.getElementById("image");
-image.addEventListener("click",changeImage);
+image.addEventListener("click", changeImage);
