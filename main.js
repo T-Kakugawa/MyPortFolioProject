a86ff607a637693fc78a7210bbf28c632a1d8d09
T-Kakugawa <kakugawatakayuki@gmail.com>
2023-08-09 14:40:37 +0900

    add changeImage()

---
+++ main.js
@@ -0,0 +1,10 @@
+//画像を変更するやつ
+function changeImage() {
+    let images = ["../CompanyHPwithFlexBox/resources/images/shiba.jpeg",
+        "../CompanyHPwithFlexBox/resources/images/shiba3.jpeg",
+        "../CompanyHPwithFlexBox/resources/images/shiba4.jpeg",
+        "../CompanyHPwithFlexBox/resources/images/草原.jpeg"];
+    const image = document.getElementById("image");
+    let randomIndex = Math.floor(Math.random() * images.length);
+    image.src = images[randomIndex];
+}
